fb97c50a19e2f2f940fab3d1c12e743bf25e3f0e
Bektur <Akmatovmak99Gmail.com>
2025-04-01 14:30:28 +0600

    Create-new-branch

--- src/main/java/talentLMS/driverPage/ChromeWebDriver.java
+++ src/main/java/talentLMS/driverPage/ChromeWebDriver.java
@@ -1,4 +1,17 @@
 package talentLMS.driverPage;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class ChromeWebDriver {
+public static WebDriver loadChromeWebDriver(){
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    return driver;
+}
 }
--- src/main/java/talentLMS/driverPage/MainDriver.java
+++ src/main/java/talentLMS/driverPage/MainDriver.java
@@ -1,4 +1,32 @@
 package talentLMS.driverPage;
 
+import org.openqa.selenium.WebDriver;
+import talentLMS.fileUtils.ConfigReader;
+
 public class MainDriver {
+    private MainDriver(){
+    }
+    static WebDriver driver;
+    public static WebDriver getDriver(){
+        if (driver==null){
+            switch (ConfigReader.getProperty("browserType").toLowerCase()){
+                case"chrome":
+                    driver = ChromeWebDriver.loadChromeWebDriver();
+                    break;
+                case "edge":
+                    driver =EdgeWebDriver.loadEdgeDriver();
+                    break;
+                case "firefox":
+                    driver = FireFoxWebDriver.loadFireFoxDriver();
+                    break;
+                case "safari":
+                    driver =SafariWebDriver.loadSafariDriver();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Uncorrected browser type" + ConfigReader.getProperty("browserType") );
+
+            }
+        }
+        return driver;
+    }
 }
