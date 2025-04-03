42e937fd61c86dc6649d93c84440251bd177dd2c
Bektur <Akmatovmak99Gmail.com>
2025-04-03 17:24:23 +0600

    Create sub button

--- src/main/java/talentLMS/pages/HomePage.java
+++ src/main/java/talentLMS/pages/HomePage.java
@@ -86,6 +86,114 @@ public class HomePage {
     @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Account & Settings\"]")
     public WebElement textAccountAndSettings;
 
+    @FindBy(xpath ="(//div[@data-toggle=\"buttons-radio\"])[position()=1]/a[normalize-space(text())=\"Today\"]" )
+    public WebElement btnInDiagramToday;
+
+    @FindBy(xpath = "(//div[@data-toggle=\"buttons-radio\"])[position()=1]/a[normalize-space(text())=\"Yesterday\"]")
+    public WebElement btnInDiagramYesterday;
+
+    @FindBy(xpath = "(//div[@data-toggle=\"buttons-radio\"])[position()=1]/a[normalize-space(text())=\"Week\"]")
+    public WebElement btnInDiagramWeek;
+
+    @FindBy(xpath = "(//div[@data-toggle=\"buttons-radio\"])[position()=1]/a[normalize-space(text())=\"Month\"]")
+    public WebElement btnInDiagramMonth;
+
+    @FindBy(id="tl-admin-right-sidebar-show-timeline")
+    public WebElement btnShowTimeLine;
+
+    @FindBy(id="tl-admin-right-sidebar-show-chart")
+    public WebElement btnShowChart;
+
+    @FindBy(id="tl-admin-right-sidebar-show-statistics")
+    public WebElement btnShowStatistic;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add user\"]")
+    public WebElement subBtnAddUser;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add course\"]")
+    public WebElement subBtnAddCourse;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Course Store\"]")
+    public WebElement subBtnCourseStore;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add category\"]")
+    public WebElement subBtnAddCategory;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add group\"]")
+    public WebElement subBtnAddGroup;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add branch\"]")
+    public WebElement subBtnAddBranch;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add notification\"]")
+    public WebElement subBtnAddNotification;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add automation\"]")
+    public WebElement subBtnAddAutomation;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Add user type\"]")
+    public WebElement subBtnAddUserType;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Import\"]")
+    public WebElement subBtnImport;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Export\"]")
+    public WebElement subBtnExport;
+
+    @FindBy(xpath = "//div[@id=\"tl-admin-dashboard-reports\"]/div/div[2]/a[normalize-space(text())=\"Users\"]")
+    public WebElement subBtnUsersInReports;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Courses\"]")
+    public WebElement subBtnCourses;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Branches\"]")
+    public WebElement subBtnBranches;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Groups\"]")
+    public WebElement subBtnGroups;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Scorm\"]")
+    public WebElement subBtnScorm;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Tests\"]")
+    public WebElement subBtnTests;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Infographics\"]")
+    public WebElement subBtnInfographics;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Training matrix\"]")
+    public WebElement subBtnTrainingMatrix;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Custom\"]")
+    public WebElement subBtnCustom;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Homepage\"]")
+    public WebElement subBtnHomepage;
+
+    @FindBy(xpath = "//div[@id=\"tl-admin-settings\"]/div[2]/a[normalize-space(text())=\"Users\"]")
+    public WebElement subBtnUsers;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Themes\"]")
+    public WebElement subBtnThemes;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Certificates\"]")
+    public WebElement subBtnCertificates;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Gamification\"]")
+    public WebElement subBtnGamification;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"E-commerce\"]")
+    public WebElement subBtnE_commerce;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Domain\"]")
+    public WebElement subBtnDomain;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a[normalize-space(text())=\"Subscription\"]")
+    public WebElement subBtnSubscription;
+
+    @FindBy(xpath = "//div[@class=\"hidden-phone\"]/a/strong")
+    public WebElement subBtnNewInterface;
+
 
 
 
