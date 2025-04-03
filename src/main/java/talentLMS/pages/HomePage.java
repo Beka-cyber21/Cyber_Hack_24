a53064c8621b8f99a2119440d592a5cc5599684b
Bektur <Akmatovmak99Gmail.com>
2025-04-03 11:00:31 +0600

    Create administrstor page

--- src/main/java/talentLMS/pages/HomePage.java
+++ src/main/java/talentLMS/pages/HomePage.java
@@ -56,5 +56,170 @@ public class HomePage {
     @FindBy(xpath = "(//i[@class=\"icon-circle tl-icon-stack-background\"])[position()=10]")
     public WebElement accountAndSettingsIcon;
 
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Users\"]")
+    public WebElement textUsers;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Courses\"]")
+    public WebElement textCourses;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Categories\"]")
+    public WebElement textCategories;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Groups\"]")
+    public WebElement textGroups;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Branches\"]")
+    public WebElement textBranches;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Events engine\"]")
+    public WebElement textEventEngine;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"User types\"]")
+    public WebElement textUserTypes;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Import - Export\"]")
+    public WebElement textImport_Export;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Reports\"]")
+    public WebElement textReports;
+
+    @FindBy(xpath = "//div[@class=\"tl-bold-link\"]/a[normalize-space(text())=\"Account & Settings\"]")
+    public WebElement textAccountAndSettings;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
